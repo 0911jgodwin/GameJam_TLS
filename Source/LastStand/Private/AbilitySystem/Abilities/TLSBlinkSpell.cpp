c5248ac3068253f8bc52d1ceff2d36350da298fb
0911jgodwin <exogenesis96@gmail.com>
2024-05-27 18:09:39 +0100

    Setup basic blink ability

---
+++ Source/LastStand/Private/AbilitySystem/Abilities/TLSBlinkSpell.cpp
@@ -0,0 +1,22 @@
+// Copyright JGodwin
+
+
+#include "AbilitySystem/Abilities/TLSBlinkSpell.h"
+
+void UTLSBlinkSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                     const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                     const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UTLSBlinkSpell::BlinkCharacter(const FVector& BlinkDirection, const float& BlinkDistance)
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	if (!bIsServer) return;
+	
+	const FVector StartLocation = GetAvatarActorFromActorInfo()->GetActorLocation();
+	
+	
+	GetAvatarActorFromActorInfo()->SetActorLocation(StartLocation+(BlinkDirection*BlinkDistance));
+}
---
+++ Source/LastStand/Public/AbilitySystem/Abilities/TLSBlinkSpell.h
@@ -0,0 +1,21 @@
+// Copyright JGodwin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/TLSGameplayAbility.h"
+#include "TLSBlinkSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTSTAND_API UTLSBlinkSpell : public UTLSGameplayAbility
+{
+	GENERATED_BODY()
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Blink")
+	void BlinkCharacter(const FVector& BlinkTargetLocation, const float& BlinkDistance);
+};
